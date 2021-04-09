01718efa2d47e9b5c5d962e6f279e91e7f2b966e
John Common <johncommon@Johns-MacBook-Air.local>
2021-04-09 08:27:26 -0700

    completed exercide and added a few things like an age calculator

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -15,6 +15,12 @@ public class Exercise_03 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("I am having fun with this and the command line doing everything there is crazy! ");
+        int YearBorn = 1957;
+        int CurrentYear = 2021;
+        int Age = CurrentYear - YearBorn;
+        System.out.println ("You are "+Age+" Years Old");
+
 
         // write code to print out "Check out my first program!" below
 
